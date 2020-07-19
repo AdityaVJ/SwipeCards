cfcd6d3db2992e10cdd4c80457cc99adf9cd2e70
Aditya V Jajodia <aditya.v.jajodia@gmail.com>
2020-07-19 14:33:45 +0530

    Refactored ApiService interface to include RxJava Observable. Added CallAdapterFactory to the WebsService singleton. Basic layout changes made; will be refactored later.

--- app/src/main/java/com/jajodia/swipecards/api/ApiService.kt
+++ app/src/main/java/com/jajodia/swipecards/api/ApiService.kt
@@ -1,11 +1,12 @@
 package com.jajodia.swipecards.api
 
 import com.jajodia.swipecards.models.ApiResponseModel
+import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiService {
 
     @GET("fjaqJ")
-    fun getData(): Call<ApiResponseModel>
+    fun getData(): Observable<ApiResponseModel>
 }
--- app/src/main/java/com/jajodia/swipecards/api/WebService.kt
+++ app/src/main/java/com/jajodia/swipecards/api/WebService.kt
@@ -3,6 +3,7 @@ package com.jajodia.swipecards.api
 import com.jajodia.swipecards.Constants
 import okhttp3.*
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
@@ -10,18 +11,19 @@ import java.util.concurrent.TimeUnit
 object WebService {
 
     private val httpClient: OkHttpClient = OkHttpClient
-            .Builder()
-            .connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .build()
+        .Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
 
     private val retrofit: Retrofit = Retrofit
-            .Builder()
-            .client(httpClient)
-            .baseUrl(Constants.API)
-            .addConverterFactory(MoshiConverterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        .Builder()
+        .client(httpClient)
+        .baseUrl(Constants.API)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
 
     val apiService: ApiService = retrofit.create(ApiService::class.java)
 }
