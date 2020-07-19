898e0279e18318ed4423898ceda0cc6f958abea5
Aditya V Jajodia <aditya.v.jajodia@gmail.com>
2020-07-19 14:08:31 +0530

    Initial project setup with basic libraries and package structuring done.

---
+++ app/src/main/java/com/jajodia/swipecards/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jajodia.swipecards.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jajodia.swipecards.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
